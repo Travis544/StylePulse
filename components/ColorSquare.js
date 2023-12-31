d5bdf5dbe597b7ae95203460af639b5b16202d85
travis544 <traviszheng544@gmail.com>
2023-12-31 15:26:54 -0800

    Fixed color match

--- components/ColorSquare.js
+++ components/ColorSquare.js
@@ -1,16 +1,27 @@
 import { View, Text, StyleSheet, ScrollView, Image, FlatList, } from 'react-native';
 export default function ColorSquare({ colorData }) {
     return (
+
+
         <View style={{
-            backgroundColor: colorData.hexValue,
-            flex: 1,
-            width: 100,
-            height: 100,
+            width: 200,
+            height: 200,
+            backgroundColor: "white",
+            flex: 1
         }}>
+            <View style={{
+                backgroundColor: colorData.hexValue,
+                flex: 1,
+                width: "100%",
+                height: "100%",
+            }}>
+
+            </View>
             <Text style={styles.colorText}>
                 {colorData.color}
             </Text>
         </View>
+
     )
 }
 
